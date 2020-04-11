e72e24a677dde40b29f2ebb0aef5aa1bc28a21de
Louis Laureys <louis@laureys.me>
2020-04-11 22:58:17 +0200

    Fix create-apikey command

--- src/api-keys/api-keys.module.ts
+++ src/api-keys/api-keys.module.ts
@@ -3,15 +3,18 @@ import { JwtModule } from '@nestjs/jwt'
 import { TypeOrmModule } from '@nestjs/typeorm'
 import { ConfigModule } from 'src/config/config.module'
 import { ConfigService } from 'src/config/config.service'
+import { UsersModule } from 'src/users/users.module'
 
 import { ApiKey } from './api-key.entity'
+import { ApiKeysCli } from './api-keys.cli'
 import { ApiKeysController } from './api-keys.controller'
 import { ApiKeysService } from './api-keys.service'
 
 @Module({
   controllers: [ApiKeysController],
-  providers: [ApiKeysService],
+  providers: [ApiKeysService, ApiKeysCli],
   imports: [
+    UsersModule,
     TypeOrmModule.forFeature([ApiKey]),
     JwtModule.registerAsync({
       imports: [ConfigModule],
