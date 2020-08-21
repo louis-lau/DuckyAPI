47482c3984511e85d5f03f54bbf1546826915d4d
Louis Laureys <louis@laureys.me>
2020-08-21 12:59:07 +0200

    Fixes #10

--- webpack.config.prod.js
+++ webpack.config.prod.js
@@ -1,4 +1,4 @@
-const merge = require('webpack-merge')
+const { merge } = require('webpack-merge')
 const baseConfig = require('./webpack.config.base.js')
 const nodeExternals = require('webpack-node-externals')
 
